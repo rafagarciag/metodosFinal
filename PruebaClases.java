d0e5461cb9d9d50de7aaf3d6e7281265938a139f
Guayo <higgg.1989@gmail.com>
2011-11-25 19:04:41 -0600

    Tarea y clase de prueba

---
+++ PruebaClases.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class PruebaClases{
+
+	public static void main (String [] args){
+		Tarea t = new Tarea(1,2);
+		
+		System.out.println("Hello World");
+		System.out.println("ID:"+t.getId());
+		System.out.println("Num: "+t.getNum());
+	}
+
+
+}
