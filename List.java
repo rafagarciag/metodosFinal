a0f67da9b3feeb7061941f4f01822c40f0b3eaca
Alan <omgitsalan@gmail.com>
2011-11-26 22:46:07 -0600

    arreglado List

--- List.java
+++ List.java
@@ -7,8 +7,7 @@ public class List extends Broker{
         for(int i=0; i<t.getNum(); i++){
 			
 			if(Simulacion.procesadores[i].isEmpty()){
-				//Hacer set de departure
-				
+				Simulacion.procesadores[i].setNextDeparture(StdRandom.exp(Simulacion.mu));				
 			}
 
 				Simulacion.procesadores[i].pushTarea(t);
